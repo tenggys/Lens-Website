855b4b7c4b82e1c9aeb8ae2566d8cc2c5cded136
tenggys <levgood21@yandex.ru>
2024-01-25 15:25:13 +0300

    доработка

--- approachCard.js
+++ approachCard.js
@@ -1,8 +1,9 @@
 const approachBox = document.querySelector('#approach-box');
 
+getApproachs();
 
 async function getApproachs() {
-    const response = await fetch('approachCard.jsn');
+    const response = await fetch('approachCard.json');
     const approachArray = await response.json();
 
     renderApproachs(approachArray);
@@ -10,7 +11,12 @@ async function getApproachs() {
 
 function renderApproachs(approachArray) {
     approachArray.forEach(function (item) {
-        const approachHTML ='';
+        const approachHTML =`
+            <div class="approach__card" data-id="${item.id}">
+                <div class="approach__card-title"><span>${item.number}</span> ${item.title}</div>
+                <div class="approach__card-text">${item.text}</div>
+            </div>
+        `;
         approachBox.insertAdjacentHTML('beforeend', approachHTML)
     });
 };
